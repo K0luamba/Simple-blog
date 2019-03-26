9af6e7cf787701c6f03c4e1d277003696b6ab88c
Matfred <kolya.reshetnik@yandex.ru>
2019-03-26 23:22:55 +0300

    templates to show posts from database

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request): #вызывается при начальной странице
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts' : posts}) #в {} то, что передаем как параметр в отображение страицы
