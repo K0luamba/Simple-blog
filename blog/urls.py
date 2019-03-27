bae89566b0eee89df3de5576ce423e3305c65c2b
Matfred <kolya.reshetnik@yandex.ru>
2019-03-28 00:22:06 +0300

    Added CSS and unique pages with post details

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 #используем именованный URL и определяем направление запроса для начальной страницы
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def post_list(request): #вызывается при начальной странице
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts' : posts}) #в {} то, что передаем как параметр в отображение страицы
+
+def post_detail(request, pk): #при передаче запроса сохраняется значение pk
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post}) #передаем только один пост
